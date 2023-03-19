1b90b967539561da384fd20004bbdfb54819d37a
mkadr5 <muhammetkadir52@gmail.com>
2023-03-19 13:43:26 +0300

    Initial Commit

---
+++ ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by Muhammet Kadir on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
